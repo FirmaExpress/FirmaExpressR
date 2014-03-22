ae65670ae0a77e3d740a69f56c9b91fdd2201cdd
Claudio Guerra <claudevandort@gmail.com>
2014-03-22 18:46:24 -0300

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  def index
+  	
+  end
+
+  def login
+  	
+  end
+  def register
+
+  end
+  def aboutus
+  	
+  end
+  def plans
+  	
+  end
+  def contact
+    
+  end
+end
