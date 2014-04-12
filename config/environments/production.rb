a48e53988ad97a12cb691e17860bd6c886e0105a
Claudio Guerra <claudevandort@gmail.com>
2014-04-11 21:17:20 -0300

    production env configuration changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,15 @@ FirmaExpressR::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,4 +78,18 @@ FirmaExpressR::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'firmaexpress2014@gmail.com'}
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'firmaexpress.com',
+    user_name:            'firmaexpress2014',
+    password:             'zaq12wsx@',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
