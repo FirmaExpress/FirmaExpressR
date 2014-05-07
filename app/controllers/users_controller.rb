0049d370b13b49a0fefe0f151e88384e1d7b2cfa
Claudio Guerra <claudevandort@gmail.com>
2014-05-07 12:13:37 -0400

    add invitations methods to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!, except: [:complete_invitee_profile]
+	before_action :invitations, :invitations_left
 	#before_action :check_auth, only: [:profile, :invite]
 	def new
 		@user = User.new
