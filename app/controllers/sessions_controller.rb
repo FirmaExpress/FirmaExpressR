e5d45a67263119abb94ca3a8c4b6af6a9f5dbbc3
patricio <patricio@patricio-Satellite-L515.(none)>
2014-05-09 19:21:24 -0400

    Redirect to sign-in Ready

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 			session[:user_id] = user.id
 			redirect_to root_url, :notice => "Logged in!"
 		else
-			flash.now.alert = "Invalid email or password"
+			flash.now.alert = "Su email o password es Incorrecto"
 			render "new"
 		end
 	end
