2f287228294021fa4da02424a97257ca55a3afa2
Claudio Guerra <claudevandort@gmail.com>
2014-05-15 13:20:58 -0400

    document_controller auth_user, except :check

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,5 @@
 class DocumentsController < ApplicationController
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:check]
 	before_action :invitations, :invitations_left
 	#before_action :check_auth, except: [:check]
 	def new
