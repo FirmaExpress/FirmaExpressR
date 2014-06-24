490306f04de2e557eb5dfc7af551cd1fd4cc391e
Claudio Guerra <claudevandort@gmail.com>
2014-06-24 07:47:21 -0400

    add migration for name and drescription in security levels

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618004641) do
+ActiveRecord::Schema.define(version: 20140623212207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20140618004641) do
     t.integer  "level"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "description"
   end
 
   create_table "sign_security_methods", force: true do |t|
