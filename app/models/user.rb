16c2c2a70eabd741a36419e5a85ed362f063ff0e
Claudio Guerra <claudevandort@gmail.com>
2014-08-20 01:08:24 -0400

    Antes de validar usuario, el modelo de usuario debe obtener el nombre de la persona usando su rut

--- app/models/user.rb
+++ app/models/user.rb
@@ -97,6 +97,18 @@ class User < ActiveRecord::Base
 
 	def set_user_id
 		self.user_type_id = 2 if self.user_type_id.blank?
+		
+		rut, dv = id_number.split('-')
+		rut = rut.delete "."
+		sii_page = Nokogiri::HTML(open("https://zeus.sii.cl/cvc_cgi/stc/getstc?RUT=#{rut}&DV=#{dv}&PRG=STC&OPC=NOR"))
+		begin
+			full_name = sii_page.css('html body center')[1].css('table')[0].css('tr')[0].css('td')[1].css('font').text.strip.titleize
+			self.first_name = full_name.match(/^([^ ]*\ [^ ]*)\ (.*)$/)[1]
+			self.last_name = full_name.match(/^([^ ]*\ [^ ]*)\ (.*)$/)[2]
+		rescue Exception => e
+			self.first_name = ''
+			self.last_name = ''
+		end
 	end
 
 	def self.authenticate(email, password)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ free = UserType.create name: 'Invitado'
 pending = UserType.create name: 'Pendiente'
 
 User.create([
-	{ avatar: nil, first_name: 'Claudio', last_name: 'Guerra', id_number: '17026575-0', email: 'claudevandort@gmail.com', password: '12345678', password_confirmation: '12345678',user_type: admin },
-	{ avatar: nil, first_name: 'Patricio', last_name: 'Jara', id_number: '17121986-8', email: 'patricioalfredo18@gmail.com', password: '12345678', password_confirmation: '12345678',user_type: admin },
+	#{ avatar: nil, first_name: 'Claudio', last_name: 'Guerra', id_number: '17026575-0', email: 'claudevandort@gmail.com', password: '12345678', password_confirmation: '12345678',user_type: admin },
+	#{ avatar: nil, first_name: 'Patricio', last_name: 'Jara', id_number: '17121986-8', email: 'patricioalfredo18@gmail.com', password: '12345678', password_confirmation: '12345678',user_type: admin },
 	{ avatar: nil, first_name: 'Daniel', last_name: 'Vera', id_number: '10974805-6', email: 'danielveram@gmail.com', password: 'guitarra', password_confirmation: 'guitarra',user_type: admin },
 	{ avatar: nil, first_name: 'Diego', last_name: 'Maturana', id_number: '2-7', email: 'diegomaturanabaeza@gmail.com', password: 'D13g0M4tur4n4', password_confirmation: 'D13g0M4tur4n4',user_type: admin },
 	{ avatar: nil, first_name: 'Francisco', last_name: 'Masjuan', id_number: '1-9', email: 'fmasjuan@co-munity.co', password: 'Fr4nc1sc0', password_confirmation: 'Fr4nc1sc0',user_type: admin }])
