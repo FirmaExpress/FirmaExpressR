6eed45be6e3453eefadd1a57a9cc9843dda74783
Claudio Guerra <claudevandort@gmail.com>
2014-09-09 09:09:35 -0300

    pp c

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,6 +1,13 @@
 PayPal::Recurring.configure do |config|
-	config.sandbox = true
-	config.username = 'claudio.guerra-facilitator_api1.firmaexpress.com'
-	config.password = '4ERXC6ABLTDE4N4V'
-	config.signature = 'AHlPGNepMFDOcq11cW7hr-OsduMIAva5dxJyI9IEOAgOuFbDAfckFX9T'
+	if Rails.env.production?
+		config.sandbox = false
+		config.username = 'claudio.guerra_api1.firmaexpress.com'
+		config.password = '6L7V8KPWZ78G3Z3G'
+		config.signature = 'AiLmO7bn-mivROZZ1IkMcsZQuy0vAhnkNZtAM7fHuanP7GjUgf9Q.JlC'
+	else
+		config.sandbox = true
+		config.username = 'claudio.guerra-facilitator_api1.firmaexpress.com'
+		config.password = '4ERXC6ABLTDE4N4V'
+		config.signature = 'AHlPGNepMFDOcq11cW7hr-OsduMIAva5dxJyI9IEOAgOuFbDAfckFX9T'
+	end
 end
