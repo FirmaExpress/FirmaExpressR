bc8865b875e01aaebd58748eb2afdbd933057f79
Claudio Guerra <claudevandort@gmail.com>
2015-04-15 18:11:47 -0300

    .

--- docker-build.sh
+++ docker-build.sh
@@ -9,12 +9,12 @@ ln ~/Github/ssl/FirmaExpressR/firmaexpress_com.ca-bundle docker/rails/production
 ln ~/Github/ssl/FirmaExpressR/firmaexpress_com.crt docker/rails/production/ssl/firmaexpress_com.crt
 ln ~/Github/ssl/FirmaExpressR/myserver.key docker/rails/production/ssl/myserver.key
 ln ~/Github/ssl/FirmaExpressR/server.csr docker/rails/production/ssl/server.csr
-docker build -t patriciojara/firmaexpress-pg:dev docker/pg
-docker build -t patriciojara/firmaexpress-rails:dev docker/rails/development
+#docker build -t patriciojara/firmaexpress-pg:dev docker/pg
+#docker build -t patriciojara/firmaexpress-rails:dev docker/rails/development
 docker build -t patriciojara/firmaexpress-rails:prod docker/rails/production
 rm docker/rails/development/Gemfile docker/rails/development/Gemfile.lock docker/rails/production/Gemfile docker/rails/production/Gemfile.lock
 rm -R docker/rails/production/ssl
 #docker run -d -p 127.0.0.1:80:80 --name rails rails
-docker push patriciojara/firmaexpress-pg:dev
-docker push patriciojara/firmaexpress-rails:dev
+#docker push patriciojara/firmaexpress-pg:dev
+#docker push patriciojara/firmaexpress-rails:dev
 docker push patriciojara/firmaexpress-rails:prod
