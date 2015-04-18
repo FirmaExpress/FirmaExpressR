b4a23b29cefdd6c2a45086758e187493e394eb9a
Claudio Guerra <claudevandort@gmail.com>
2015-04-18 20:34:55 -0300

    fix run-prod

--- docker-run-prod.sh
+++ docker-run-prod.sh
@@ -5,9 +5,9 @@ service apache2 stop; service postgresql stop
 #rake db:seed
 #docker run -d -p 127.0.0.1:5432:5432 --name simplecases-pg-dev claudevandort/simplecases-pg:dev
 mkdir tmp; chmod -R 777 tmp; chmod -R 777 public;
-#docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name firmaexpress-rails-prod patriciojara/firmaexpress-rails:prod
+docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name firmaexpress-rails-prod patriciojara/firmaexpress-rails:prod
 #docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name firmaexpress-rails-cron-prod patriciojara/firmaexpress-rails:prod /bin/bash -l -c "service postfix restart; whenever --update-crontab; cron -f"
 #docker run -d --net="host" -v $PWD:/var/www/webapp:rw --name simplecases-rails-prod claudevandort/simplecases-rails:prod /bin/bash -l -c "rake assets:precompile RAILS_ENV=production; whenever --update-crontab"; /usr/sbin/apache2 -D FOREGROUND
-docker run --net="host" -v $PWD:/var/www/webapp:rw --name firmaexpress-rails-prod-i -i -t patriciojara/firmaexpress-rails:prod /bin/bash -l
+#docker run --net="host" -v $PWD:/var/www/webapp:rw --name firmaexpress-rails-prod-i -i -t patriciojara/firmaexpress-rails:prod /bin/bash -l
 #docker run --name simplecases-pg-dev -i -t claudevandort/simplecases-pg:dev /bin/bash -l
 #rake assets:clobber
